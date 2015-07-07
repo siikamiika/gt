4f09118669f5190c6f0584ff7b8fbdb06e47fbc5
shdown <shdownnine@gmail.com>
2015-07-07 21:35:29 +0300

    tests/test_gt.py: add

---
+++ tests/test_gt.py
@@ -0,0 +1,43 @@
+import unittest
+
+try:
+    import gt
+except ImportError: # hack for pylint
+    from .. import gt # pylint: disable=import-self
+
+class GTLibrary(unittest.TestCase):
+    translation = None
+
+    def setUp(self):
+        self.translation = gt.get_translation(
+            'en', 'ru', 'hello',
+            include_translit=True, include_variants=True,
+            include_segments=True, include_examples=True,
+            include_definitions=True, include_see_also=True,
+            include_synonyms=True, suggest_language=True,
+            correct_typos=True, interface_lang='ru')
+
+    def test_original(self):
+        self.assertEqual(
+            self.translation.original,
+            'hello')
+
+    def test_translation(self):
+        self.assertEqual(
+            self.translation.translation.lower(),
+            'здравствуйте')
+
+    def test_translation_translit(self):
+        self.assertEqual(
+            self.translation.translation_translit,
+            'zdravstvuyte')
+
+    def test_original_translit(self):
+        self.assertEqual(
+            self.translation.original_translit,
+            'heˈlō,həˈlō')
+
+    def test_source_lang(self):
+        self.assertEqual(
+            self.translation.source_lang,
+            'en')
