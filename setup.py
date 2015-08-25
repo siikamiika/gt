1ec53a0dc774f0d865a7bd2e8bc59cd408b5d601
shdown <shdownnine@gmail.com>
2015-08-25 19:00:04 +0300

    Remove VERSION, add gt/version.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
-VERSION = open('VERSION', 'r').read().strip()
+# Get the version from gt/version.py without importing the package
+exec(compile(open('gt/version.py').read(), 'gt/version.py', 'exec'))
 
 setup(name='gt',
-      version=VERSION,
+      version=__version__,
       description='Google Translate library and clients',
       long_description='''\
 Includes a library for Google Translate (gt), a console client (gt_console), an
@@ -11,7 +12,7 @@ xsel+libnotify client (gt_notify) and some other utilites.''',
       author='shdown',
       author_email='shdownnine@gmail.com',
       url='https://github.com/shdown/gt',
-      download_url='https://github.com/shdown/gt/tarball/v' + VERSION,
+      download_url='https://github.com/shdown/gt/tarball/v' + __version__,
       license='LGPLv3',
       packages=['gt'],
       scripts=['gt_console', 'gt_notify', 'gt_dump_json', 'gt_play',
