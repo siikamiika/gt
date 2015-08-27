45e8bc6c0f24277c71b2f7f784787de3d49eddd5
shdown <shdownnine@gmail.com>
2015-08-27 11:00:54 +0300

    gt/json_preproc: fix grammar and such

--- gt/json_preproc.py
+++ gt/json_preproc.py
@@ -11,7 +11,7 @@ class ParserState:
     NORMAL = 1
     # Going to append 'null' if the next symbol is comma
     COMMA = 2
-    # We're inside a string; going to wait for an unescaped comma
+    # We're inside a string; waiting for an unescaped comma
     STRING = 3
     # We're inside a string, previous character is a backslash
     STRING_ESCAPE = 4
@@ -20,7 +20,7 @@ def preprocess(source):
     """
     Converts JavaScript-compatible nested arrays to a valid JSON.
     The only known differences are:
-        - omitting of 'null' inside arrays
+        - omission of 'null' inside arrays
         - an optional meaningless comma before ']'
 
     Args:
